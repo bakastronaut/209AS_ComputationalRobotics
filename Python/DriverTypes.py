b88dd99f35f0a94ddeceb2bc632c4b0dc549e5ff
bakastronaut <bakarihassan@ucla.edu>
2018-10-21 16:04:12 -0700

    Updating driver and vehicle types

--- Python/DriverTypes.py
+++ Python/DriverTypes.py
@@ -1,23 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Sun Oct 21 15:50:06 2018
+Created on Sun Oct 21 16:02:45 2018
 
 @author: johndoe
 """
 
-class Sedan:
+class Passive:
+    def __init__(self):
+        pass
+    
+class Moderate:
     def __init__(self):
         pass
 
-class Compact:
-    def __init__(self):
-        pass
-
-class Emergency:
-    def __init__(self):
-        pass
-
-class Semi:
+class Aggressive:
     def __init__(self):
         pass
