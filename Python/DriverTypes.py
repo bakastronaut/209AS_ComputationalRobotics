60684230ef6684ab72c2b86cf4a5b25c3c7b1a7e
bakastronaut <bakarihassan@ucla.edu>
2018-10-21 16:55:34 -0700

    Building-out vehicle and driver classes

--- Python/DriverTypes.py
+++ Python/DriverTypes.py
@@ -4,16 +4,54 @@
 Created on Sun Oct 21 16:02:45 2018
 
 @author: johndoe
+
+Classes:
+    Passive
+    Moderate
+    Aggressive
+
+Attributes:
+    PREFERRED_SPEED_RATIO_MEAN:
+        The driver's speed preference as a fraction of the speed limit.
+    PREFERRED_SPEED_RATIO_STDDEV:
+        Standard deviation in speed ratios for driver of that type.
+    
+Methods:
+    
+    
+Functions:
+    speed_ratio_calculate
+    
 """
+
+import numpy as np
+
+def speed_ratio_calculate(mean,stddev):
+        return stddev * (np.random.randn() + mean)
 
 class Passive:
     def __init__(self):
-        pass
+        self.PREFERRED_SPEED_RATIO_MEAN = 0.70
+        self.PREFERRED_SPEED_RATIO_STDDEV = 0.15
+        
+        mean = self.PREFERRED_SPEED_RATIO_MEAN
+        stddev = self.PREFERRED_SPEED_RATIO_STDDEV
+        self.PREFERRED_SPEED_RATIO = speed_ratio_calculate(mean,stddev)
     
 class Moderate:
     def __init__(self):
-        pass
-
+        self.PREFERRED_SPEED_RATIO = 0.80
+        self.PREFERRED_SPEED_RATIO_STDDEV = 0.1
+        
+        mean = self.PREFERRED_SPEED_RATIO_MEAN
+        stddev = self.PREFERRED_SPEED_RATIO_STDDEV
+        self.PREFERRED_SPEED_RATIO = speed_ratio_calculate(mean,stddev)
+        
 class Aggressive:
     def __init__(self):
-        pass
+        self.PREFERRED_SPEED_RATIO = 1
+        self.PREFERRED_SPEED_RATIO_STDDEV = 0.1
+        
+        mean = self.PREFERRED_SPEED_RATIO_MEAN
+        stddev = self.PREFERRED_SPEED_RATIO_STDDEV
+        self.PREFERRED_SPEED_RATIO = speed_ratio_calculate(mean,stddev)
--- Python/VehicleTypes.py
+++ Python/VehicleTypes.py
@@ -11,12 +11,17 @@ Classes:
     Semi
 
 Attributes:
-    LENGTH_HALF -   half the length of the vehicle (units and values are TBD)
-    DIST_STANDOFF - the 'safe' stand-off distance, based on the type and size,
-                    which will be used for trailing distance and to define 
-                    'cutting-off' someone when lane changing. Semis and
-                    emergency vehicles will need more space than sedans and 
-                    compacts.
+    LENGTH_HALF:        
+        Half the length of the vehicle (units and values are TBD)
+    
+    DISTANCE_STANDOFF:  
+        The 'safe' stand-off distance, based on the type and size, which will 
+        be used for trailing distance and to define 'cutting-off' someone when 
+        lane changing. Semis and emergency vehicles will need more space than 
+        sedans and compacts. This could be a base value that can be scaled or 
+        modified to be dependent upon relative velocities.
+
+Methods:
 
 """
 
