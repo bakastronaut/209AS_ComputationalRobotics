10d92cb3abcc7c5b065cbf021b60082741ce209e
bakastronaut <bakarihassan@ucla.edu>
2018-10-21 16:00:30 -0700

    Rearranging folders

--- HelloWorld.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sunday Oct 21 14:43:09 2018
-
-"""
-
-print('Hello World!')
--- VehicleTypes.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Oct 21 15:50:06 2018
-
-@author: johndoe
-"""
-
-class VehicleSedan
