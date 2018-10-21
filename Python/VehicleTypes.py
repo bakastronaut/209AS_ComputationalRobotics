544f9a72b282d41d0cef68ce1b224ca7c8a35fc4
bakastronaut <bakarihassan@ucla.edu>
2018-10-21 16:13:15 -0700

    Vehicle types

--- Python/VehicleTypes.py
+++ Python/VehicleTypes.py
@@ -2,22 +2,40 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sun Oct 21 15:50:06 2018
-
 @author: johndoe
+
+Classes:
+    Sedan
+    Compact
+    Emergency
+    Semi
+
+Attributes:
+    LENGTH_HALF -   half the length of the vehicle (units and values are TBD)
+    DIST_STANDOFF - the 'safe' stand-off distance, based on the type and size,
+                    which will be used for trailing distance and to define 
+                    'cutting-off' someone when lane changing. Semis and
+                    emergency vehicles will need more space than sedans and 
+                    compacts.
+
 """
 
 class Sedan:
     def __init__(self):
-        pass
+        self.LENGTH_HALF = 5
+        self.DIST_STANDOFF = 0
 
 class Compact:
     def __init__(self):
-        pass
+        self.LENGTH_HALF = 5
+        self.DIST_STANDOFF = 0
 
 class Emergency:
     def __init__(self):
-        pass
+        self.LENGTH_HALF = 5
+        self.DIST_STANDOFF = 0
 
 class Semi:
     def __init__(self):
-        pass
+        self.LENGTH_HALF = 5
+        self.DIST_STANDOFF = 0
