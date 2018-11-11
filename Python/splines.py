071c067e42319fe0ba8d647412e38067930fc983
bakastronaut <bakarihassan@ucla.edu>
2018-11-11 00:12:32 -0800

    Updated spline.py with cubic coefficients

--- Python/splines.py
+++ Python/splines.py
@@ -7,31 +7,64 @@ Created on Mon Oct 29 02:49:22 2018
 """
 
 import numpy as np
-import matplotlib as mpl
+import matplotlib.pyplot as plt
 import scipy as sp
+import scipy.interpolate as interpolate
 
-def fit_spline(xvals,yvals,kind='cubic',demo=False):
+def fit_spline(xvals,yvals,kind='cubic',demo=False,returncubics=False):
     '''
     Receives set of x values and y values, fits a spline and returns the spline
     
     Inputs:
-        xvals   x coordinates of points to be fit
-        yvals   y coordinates of points to be fit
-        kind    spline polynomial degree
-        demo    whether to demo its output by plotting the fit
+        xvals           x coordinates of points to be fit
+        yvals           y coordinates of points to be fit
+        kind            spline polynomial degree
+        demo            whether to demo its output by plotting the fit
+        returncubics    whether to return coefficients for cubic fits
     Ouputs:
         f       function that takes x values as inputs and outputs 
                 corresponding y values of cubic spline
+        cubics  list of arrays with coefficients inside
     '''
     
+    assert len(xvals) > 4 and len(yvals) > 4, "Need at least 4 points for a cubic fit"
+    
     # Interpolation to fit the spline
-    f = sp.interpolate.interp1d(xvals,yvals,kind=kind)
+    f = interpolate.interp1d(xvals,yvals,kind=kind)
     
     # If demo is true, show a plot of the cubic function.
     if demo:
-        mpl.pyplot.scatter(xvals,yvals)
+        plt.scatter(xvals,yvals)
         x = np.linspace(np.min(xvals),np.max(xvals),100)
-        mpl.pyplot.plot(x,f(x))
-        mpl.pyplot.show()
+        plt.plot(x,f(x))
+        plt.show()
     
-    return f
+    if returncubics:
+        def f_cubic(x,a,b,c,d):
+            return a*x**3 + b*x**2 + c*x + d
+        
+        cubics = []
+        N_curves = len(xvals) - 1
+        for n in range(N_curves):
+            x = np.linspace(xvals[n],xvals[n+1],100)
+            y = f(x)
+            params,_ = sp.optimize.curve_fit(f_cubic,x,y)
+            cubics.append(params)
+        
+        return f,cubics
+    
+    else:
+        
+        return f
+
+io0 = [1,2,3,4,5,6,7,8,9]
+io1 = [7,10,2,7,6,1,1,9,0]
+func_fit,params = fit_spline(io0,io1, kind='cubic', demo=False, returncubics=True)
+
+x = np.linspace(-5,15,1000)
+for n in range(len(params)):
+    p = params[n]
+    a,b,c,d = p[0],p[1],p[2],p[3]
+    y = a * x**3 + b * x**2 + c * x + d
+    plt.plot(x,y)
+plt.scatter(io0,io1)
